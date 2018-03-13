314f9461a4eec33020284bcdb15b5556178716c8
Cristinel Ilisei <cristinel.ilisei@gmail.com>
2018-03-13 14:29:29 +0200

    chore: Add functions to create and show initial cards
    todo's:
    - function to open a card on mouse click
    - function to check if the two cards match
    - function to lock the cards in the open position
    - function to add the card to a *list* of "open" cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 /*
  * Create a list that holds all of your cards
  */
+var cards = [   
+"anchor",
+"bicycle",
+"bolt",
+"bomb",
+"cube",
+"diamond",
+"leaf",
+"paper-plane-o"];//eigth cards array
 
+var cardList = createArray(cards);
 
 /*
  * Display the cards on the page
@@ -10,8 +20,10 @@
  *   - add each card's HTML to the page
  */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+ //****************** Shuffle function from http://stackoverflow.com/a/2450976********************************
+
 function shuffle(array) {
+
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
@@ -21,7 +33,6 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
@@ -36,3 +47,81 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+//**************** function to create the initial array of cards ************************************
+
+function createArray(array)
+{
+    var retArray = [];           //array to be returned
+    for(let i = 0; i < 2; i++)   //every card appears twice on deck
+    { array.forEach(element => {
+        retArray.push(element);
+        });
+    }
+    return retArray;
+};
+
+//**************** set up the event listener for a card **********************************************
+
+document.addEventListener("click", function(event)
+{
+    if (event.target.className.toLowerCase() === "card")  
+        displayCardOpen(event.target);   
+});
+
+//**************** compare an opening card with the one previously opened ************
+
+function compareCards(card)
+{
+
+}
+
+//************************* display the card's symbol ************************************************
+
+function displayCardOpen(card){
+    card.classList.add("open");
+    card.classList.add("show");
+    card.style.cssText = "box-shadow: initial;"; //avoid keeping box-shadow as it was modified 
+}                                            //by mouseover event listener
+
+
+//************************* display the cards on the page *******************************************
+
+function display(array){
+
+    var currentIndex = array.length;
+    var cardDeck = document.getElementById("cards-board");
+
+    while (cardDeck.firstChild) {
+        cardDeck.removeChild(cardDeck.firstChild);
+    }//remove all previous cards (necessary when 'restart' was clicked)
+
+    while (currentIndex !== 0) {
+        var node = document.createElement("li"); 
+        var card = document.createElement("i");
+        node.classList.add("card");
+        card.classList = ("fa " + "fa-" + array[currentIndex-1]);
+        node.appendChild(card);
+        cardDeck.appendChild(node);
+        currentIndex -= 1;
+    }
+}
+//************ change the card's box-shadow when mouse pointer is hovering **************************
+//not in project requirements, but just for fun
+
+document.addEventListener("mouseover", function(event)
+{
+    if (event.target.className.toLowerCase() === "card") 
+        event.target.style.cssText = "box-shadow: 5px 5px 20px 0 rgba(255, 255, 0, 0.5);";
+});
+
+//*************** revert the card's box-shadow when mouse pointer is leaving **************************
+
+document.addEventListener("mouseout", function(event)
+{
+    if (event.target.className.toLowerCase() === "card") 
+        event.target.style.cssText = "box-shadow: initial;";
+});
+
+shuffle(cardList);
+display(cardList);
